9de3673f9c0f57faad36e861bda09a216812bf04
tomfriwel <976932447@qq.com>
2017-05-08 16:13:11 +0800

    Initial Commit

---
+++ TestNSUserDefaultsPerformance/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestNSUserDefaultsPerformance
+//
+//  Created by tomfriwel on 08/05/2017.
+//  Copyright © 2017 tomfriwel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestNSUserDefaultsPerformance/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestNSUserDefaultsPerformance
+//
+//  Created by tomfriwel on 08/05/2017.
+//  Copyright © 2017 tomfriwel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
